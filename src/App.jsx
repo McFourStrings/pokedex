3e80fb7ed90442e476a05d8e45e9b476b33d0355
unknown <yannmonory@gmail.com>
2026-06-17 14:21:49 +0200

    ADD basename

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename="/pokedex">
         <NavBar />
         <Routes>
           <Route path='/' element={<Pokedex />} />
